383ce2684b15ba835d0d2f2d0f94205f9bc15383
Blaza-4 <adinnavakumar22@gmail.com>
2025-06-20 23:31:39 +0530

    removed comments kinda fixed probe call : moving to read/write

--- usbDriver/usbDriver.c
+++ usbDriver/usbDriver.c
@@ -29,7 +29,9 @@ struct usb_skel{
     struct usb_interface* interface;
     struct kref kref;
     size_t bulk_in_size;
+    size_t bulk_out_size;
     unsigned char *bulk_in_buffer;
+    unsigned char *bulk_out_buffer;
     struct urv *bulk_in_urb;
     __u8 bulk_in_endpointAddr;
     __u8 bulk_out_endpointAddr;
@@ -70,24 +72,23 @@ static ssize_t usb_read (struct file *file, char *buffer, size_t count,loff_t *p
         return -ENODEV;
     
     if(count > dev->bulk_in_size){
-        printk("USB : %ld is the value of count \n",count);
+        pr_info("USB : %ld is the value of count \n",count);
         count = dev->bulk_in_size;
-        printk("USB : %ld is the value of count afterwards \n",count);
-        
+        pr_info("USB : %ld is the value of count afterwards \n",count);
     }
-
+    pr_info("USB : going to use usb_bulk_msg from  0x%02x endpoint with size of %ld \n",dev->bulk_in_endpointAddr,count);
     retval = usb_bulk_msg(dev->usbdev,
                         usb_rcvbulkpipe(dev->usbdev,dev->bulk_in_endpointAddr),
                         dev->bulk_in_buffer,
-                        min(dev->bulk_in_size,count),
+                        count,
                         &read_cnt,5000);
 
     if(retval){
-        pr_info("USB : bulk msg ERR   \n");
+        pr_err("USB : bulk msg ERR   \n");
         return retval; 
         } 
     else {
-        pr_err("USB : bulk_msgs retured OK code . . \n");
+        pr_debug("USB : bulk_msgs retured OK code . . \n");
     }
     if(copy_to_user(buffer,dev->bulk_in_buffer,read_cnt))
         {
@@ -117,102 +118,57 @@ struct usb_class_driver usb_driver={
     .fops           = &fops,
     .minor_base     = USB_MINOR
 };
-/**
- * @brief this function called as the probe function when we load our usb,inside we need to add the interface
- * and register the usb device , careful about the print statements kinda tricky
- * issue 1 : not registering the usb and getting minor number probably due to other drivers . working on it 
- * @param usb_interface structure , device id structure
- */
-/**
- * im kinda modifying the probe function beacuse we need to add some more things there like the struture which holds all usb related datas 
- * need to collect and store . so that we can do it from the probe functon we will get as much as things we need from here
- * so first we create all the structure pointers and proceed
- */
 static int usb_dev_probe (struct usb_interface *intf,const struct usb_device_id *id)
 {
-    /*structure for our usb_host_interface beacuse usb may jhave multiple endpoints and descriptors this structure represents all of them*/
     struct usb_host_interface* usb_interface_desc ;
-    /*this structure defines one endpoint not all ot them so we can find the right one for read and write*/
     struct usb_endpoint_descriptor *endpoint_in,*endpoint_out;
-    /*our brain structure*/
     struct usb_skel *dev;
     dev = kzalloc(sizeof(*dev) , GFP_KERNEL);
     if(!dev)
         return -ENOMEM;
     
     int i, ret ;
-    /**
-     * init for the kref and mutex
-     */
     kref_init(&dev->kref);
     mutex_init(&dev->io_mutex);
-    /**
-     * interface_to_usbdev(intf) gets you the struct usb_device * from the usb_interface 
-     *usb_get_dev() increments the reference count of that USB device, so it doesn’t get deallocated while your driver is using it.
-    */
     dev->usbdev = usb_get_dev(interface_to_usbdev(intf));
     dev->interface = usb_get_intf(intf);
     usb_interface_desc = intf->cur_altsetting ;
-
     printk(KERN_INFO "USB : usb probed %d :(%04X:%04X)\n" , usb_interface_desc->desc.bInterfaceNumber , id->idVendor,id->idProduct);
     printk(KERN_INFO "USB : usb end points : %02x\n",usb_interface_desc->desc.bNumEndpoints);
     printk(KERN_INFO "USB : usb interface class : %02x\n",usb_interface_desc->desc.bInterfaceClass);
-    /**
-     * @brief there may ne multiple endpoints available each with IN or OUT ops 
-     * now we are checking for the IN that is why  we loop through all endpoints till we find the bulk_in one see the loop below
-     */
-    
- 
-/**
-* inside the usb_skelton file they did something like this i dont know why they did that in the first place
-* for now i am sticking with it .
-* finding the common endpoits 
-*/
-/**
- * usb_find_common_endpoints() -- look up common endpoint descriptors
- * @alt:	alternate setting to search
- * @bulk_in:	pointer to descriptor pointer, or NULL
- * @bulk_out:	pointer to descriptor pointer, or NULL
- * @int_in:	pointer to descriptor pointer, or NULL
- * @int_out:	pointer to descriptor pointer, or NULL
- *
- * Search the alternate setting's endpoint descriptors for the first bulk-in,
- * bulk-out, interrupt-in and interrupt-out endpoints and return them in the
- * provided pointers (unless they are NULL).
- *
- * If a requested endpoint is not found, the corresponding pointer is set to
- * NULL.
- *
- * Return: Zero if all requested descriptors were found, or -ENXIO otherwise.
- */
     int retval = usb_find_common_endpoints(intf->cur_altsetting,&endpoint_in,&endpoint_out,NULL,NULL);
     if(retval){
         dev_err(&intf->dev,"USB : could not find endpoints IN and OUT");
         usb_set_intfdata(intf,NULL);
         printk(KERN_ERR "USB : err in finding common endpoits . . .\n");
         return retval;
+    }else {
+        pr_info("USB : common endpoints found \n");
     }
-    /**
-     * @brief hopefully if we got something from the endpoints we can find the bulk_size and buffer endpoint address
-     */
     dev->bulk_in_size = usb_endpoint_maxp(endpoint_in);
     dev->bulk_in_endpointAddr = endpoint_in->bEndpointAddress;
     dev->bulk_in_buffer = kmalloc(dev->bulk_in_size,GFP_KERNEL);
-    printk("USB : flag");
-
+    dev->bulk_out_size = usb_endpoint_maxp(endpoint_out);
+    dev->bulk_out_endpointAddr = endpoint_out->bEndpointAddress;
+    dev->bulk_out_buffer = kmalloc(dev->bulk_out_size , GFP_KERNEL);
+    pr_info("USB : bulk in size : %ld , bulk_in_endpoint addr : 0x%02x \n",dev->bulk_in_size,dev->bulk_in_endpointAddr);
+    pr_info("USB : bulk out size :%ld ,bulk_out_endpoint_address : 0x%02x \n" ,dev->bulk_out_size,dev->bulk_out_endpointAddr);
+    printk("USB : flag 1");
     if(!dev->bulk_in_buffer){
-       printk(KERN_ERR "USB : ERROR Finding THE bulk in buffer area. . .\n");
+       dev_err(&intf->dev, "Failed to allocate bulk_in or bulk_in_buffer\n");
+       usb_put_dev(dev->usbdev);
+       kfree(dev);
        return -ENOMEM;
     }
-    if(!dev->bulk_in_buffer){
-        usb_put_dev(dev->usbdev);
-        kfree(dev);
-        return -ENOMEM;
+    if(!dev->bulk_out_buffer){
+       dev_err(&intf->dev, "Failed to allocate bulk_out or bulk_out buffer\n");
+       usb_put_dev(dev->usbdev);
+       kfree(dev);
+       return -ENOMEM;
     }
-    dev->bulk_out_endpointAddr = endpoint_out->bEndpointAddress;
 
     usb_set_intfdata(intf,dev);
-    printk("USB : flag");
+    printk("USB : flag 2");
     ret = usb_register_dev(intf,&usb_driver);
     if(ret){
        printk(KERN_ERR "USB : Error with registering and getting minor number \n");
@@ -221,10 +177,7 @@ static int usb_dev_probe (struct usb_interface *intf,const struct usb_device_id 
     }
     return 0;
 }
-/**
- * @brief function called when the usb device is disconnected we just deregister here
- * @param usb_interface structure
- */
+
 static void usb_dev_disconnect (struct usb_interface *intf){
     usb_deregister_dev(intf,&usb_driver);
     printk(KERN_INFO "USB : disconnected usb device \n");
@@ -280,17 +233,3 @@ MODULE_AUTHOR("Adin N S <adinnavakumar22@gmail.com>");
 MODULE_DESCRIPTION("Simple linux driver module");
 MODULE_LICENSE("GPL");
 
-
-//    for(i = 0 ;i < usb_interface_desc->desc.bNumEndpoints ; i++){
-//         endpoint_in = &usb_interface_desc->endpoint[i].desc;
-        
-//         if(usb_endpoint_is_bulk_in(endpoint_in)){
-//             dev->bulk_in_size = usb_endpoint_maxp(endpoint_in);
-//             dev->bulk_in_endpointAddr = endpoint_in->bEndpointAddress;
-//             dev->bulk_in_buffer = kmalloc(dev->bulk_in_size,GFP_KERNEL);
-//             if(!dev->bulk_in_buffer)
-//                 return -ENOMEM;
-//             break;
-//         }
-        
-//     } 
